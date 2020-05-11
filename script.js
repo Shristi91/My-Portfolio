e5e7803ceb61ef94a14984b86fc4c9759d0162cd
Shristi91 <63180945+Shristi91@users.noreply.github.com>
2020-05-11 11:29:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,13 @@ $(document).ready(function () {
         $(this).toggleClass('open');
         $('.top').toggleClass('open');
     });
+    $('.top .nav_link').on('click', function () {
+        $('.menu_toggler').removeClass('open');
+        $('.top').removeClass('open');
+    });
+    $('nav a[href*="#"]').on('click', function () {
+        $('html, body').animate( keyframes: {
+            scrollTop: $($(this).attr('href')).offset().top - 100
+        }, options: 2000);
+    });
 });
